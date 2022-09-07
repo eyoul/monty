c3e6040bf47ca48a7ea06416c2a886539cb7882b
eyoul <eyoulshimeles2@gmail.com>
2022-09-07 13:32:52 -0700

    Repo

--- rotr.c
+++ rotr.c
@@ -1,28 +1,30 @@
 #include "monty.h"
 
 /**
-* rotl - rotates the first element of the stack
+* rotr - rotates the last node of a stack_t stack
 * @stack: stack head
 * @line_count: line count
 *
 * Return: void
 */
-void rotl(stack_t **stack, unsigned int line_count)
+void rotr(stack_t **stack, unsigned int line_count)
 {
-	stack_t *left;
-	stack_t *right;
+	stack_t *bottom;
+	stack_t *prev;
 
 	(void) line_count;
 	if (!stack || !*stack || !(*stack)->next)
 		return;
 
-	left = right = *stack;
+	bottom = *stack;
 
-	while (right->next) /* move the right pointer to the last node */
-		right = right->next;
-	right->next = left; /* a circle infinite linked list loop */
-	left->prev = right;
-	*stack = left->next; /* so we cut the link between the 0 and 1 element */
-	(*stack)->prev->next = NULL;
-	(*stack)->prev = NULL;
+	while (bottom->next)
+		bottom = bottom->next;
+
+	prev = bottom->prev;
+	bottom->next = *stack;
+	bottom->prev = NULL;
+	prev->next = NULL;
+	(*stack)->prev = bottom;
+	*stack = bottom;
 }
